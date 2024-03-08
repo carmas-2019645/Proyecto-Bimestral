79755dca6db0092c107e4d9465fdbe32718b8da6
Carlos Armas <carmas-2019645@kinal.edu.gt>
2024-03-08 11:49:35 -0600

    Proyecto Completo

--- src/category/category.routes.js
+++ src/category/category.routes.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import {testC, createCategory, getAllCategories, updateCategory, deleteCategory} from './category.controller.js'
+import { validateJwt, isClient, isAdmin } from '../middlewares/validate-jwt.js' 
+
 
 const api = express.Router();
 
 // Rutas para categorías
 api.get('/testC', testC);
-api.post('/create', createCategory);
+api.post('/create',[validateJwt, isAdmin], createCategory);
 api.get('/getAllCategories', getAllCategories)
 api.put('/updateCategory/:id', updateCategory)
-api.delete('/deleteCategory/:id', deleteCategory)
+api.delete('/deleteCategory/:id',[validateJwt, isAdmin], deleteCategory)
 
 export default api
 
